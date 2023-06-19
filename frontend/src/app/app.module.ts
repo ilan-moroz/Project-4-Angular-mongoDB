d8b672ad3d2eba460eb37c7b1b9686fc7549c2a1
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-06-19 19:06:20 +0300

    login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Layout/header/header.component';
 import { MainComponent } from './Layout/main/main.component';
+import { LoginComponent } from './Comps/login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, MainComponent],
+  declarations: [AppComponent, HeaderComponent, MainComponent, LoginComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
