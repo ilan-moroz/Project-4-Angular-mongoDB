574fe2e22a8ce687aedeec9efd8fdbf32d9d41a7
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-06-20 17:56:27 +0300

    regsiter comp

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './Layout/main/main.component';
+import { RegisterComponent } from './Comps/register/register.component';
 
-const routes: Routes = [{ path: '', component: MainComponent }];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { MainComponent } from './Layout/main/main.component';
 import { LoginComponent } from './Comps/login/login.component';
 import { AboutComponent } from './Comps/about/about.component';
 import { InfoComponent } from './Comps/info/info.component';
+import { RegisterComponent } from './Comps/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, MainComponent, LoginComponent, AboutComponent, InfoComponent],
+  declarations: [AppComponent, HeaderComponent, MainComponent, LoginComponent, AboutComponent, InfoComponent, RegisterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
