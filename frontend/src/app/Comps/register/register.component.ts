d25d3b0f5f7cf90aa85e2fd80b53f605a8f0d4ee
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-06-21 13:20:09 +0300

    form validation

--- frontend/src/app/Comps/register/register.component.ts
+++ frontend/src/app/Comps/register/register.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  idNumber?: number;
+  email: string = '';
+  password: string = '';
+  city: string = '';
+  street: string = '';
+  firstName: string = '';
+  lastName: string = '';
 
+  onSub(form: NgForm) {
+    if (form.valid) {
+      console.log(form.value);
+    } else {
+      form.controls['idNumber'].markAsTouched();
+      form.controls['email'].markAsTouched();
+      form.controls['password'].markAsTouched();
+      console.log('Form is not valid!');
+    }
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Layout/header/header.component';
@@ -9,10 +8,19 @@ import { LoginComponent } from './Comps/login/login.component';
 import { AboutComponent } from './Comps/about/about.component';
 import { InfoComponent } from './Comps/info/info.component';
 import { RegisterComponent } from './Comps/register/register.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, MainComponent, LoginComponent, AboutComponent, InfoComponent, RegisterComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    LoginComponent,
+    AboutComponent,
+    InfoComponent,
+    RegisterComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
