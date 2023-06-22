bcc74bcb6298995981bfc81d2eaf84618fd12130
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-06-22 18:17:34 +0300

    -

--- frontend/src/app/Components/session-modal/session-modal.component.ts
+++ frontend/src/app/Components/session-modal/session-modal.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-declare var $: any;
 
 @Component({
   selector: 'app-session-modal',
   templateUrl: './session-modal.component.html',
   styleUrls: ['./session-modal.component.css'],
 })
-export class SessionModalComponent {
-  showModal() {
-    $('#sessionModal').modal('show');
-  }
-}
+export class SessionModalComponent {}
