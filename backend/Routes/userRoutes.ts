3854c2b345a301e7739f2029f9cae439e46c3650
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-06-23 13:51:32 +0300

    check if email or id exists in database before adding new  user

--- backend/Routes/userRoutes.ts
+++ backend/Routes/userRoutes.ts
@@ -5,25 +5,41 @@ import jwt from "jsonwebtoken";
 
 const userRouter = express.Router();
 
-// Create a new customer
+// save a new customer in database and check if email or idNumber already exists
 userRouter.post(
   "/addUser",
-  (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     const newUser: User = req.body;
-    const newCustomer = new UserModel(newUser);
-    newCustomer
-      .save()
-      .then((customer) => {
-        const token = jwt.sign(
-          { id: customer._id, email: customer.email },
-          "your_secret_key",
-          { expiresIn: "2h" }
-        );
-        res.status(201).json({ customer, token });
-      })
-      .catch((error) => {
-        res.status(500).json({ error: error.message });
+    const email: string = newUser.email;
+    const idNumber: number = newUser.idNumber;
+
+    try {
+      const existingUser = await UserModel.findOne({
+        $or: [{ email: email }, { idNumber: idNumber }],
       });
+      if (existingUser) {
+        return res.status(400).json({
+          message: "User with provided email or ID number already exists",
+        });
+      }
+
+      const newCustomer = new UserModel(newUser);
+      newCustomer
+        .save()
+        .then((customer) => {
+          const token = jwt.sign(
+            { id: customer._id, email: customer.email },
+            "your_secret_key",
+            { expiresIn: "2h" }
+          );
+          res.status(201).json({ customer, token });
+        })
+        .catch((error) => {
+          res.status(500).json({ error: error.message });
+        });
+    } catch (err: any) {
+      return res.status(500).json({ error: err.message });
+    }
   }
 );
 
