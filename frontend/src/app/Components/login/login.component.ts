ea4e195c67d4c51c46cb4ce19a4c8eaf6ac9b9ff
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-06-24 19:30:03 +0300

    form validation

--- frontend/src/app/Components/login/login.component.ts
+++ frontend/src/app/Components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './../../services/auth.service';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,8 +10,10 @@ import { FormControl, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
   loggedIn: boolean = false;
 
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [Validators.required]);
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
 
   constructor(private AuthService: AuthService) {}
 
@@ -25,16 +27,27 @@ export class LoginComponent implements OnInit {
   }
 
   getEmailErrorMessage() {
-    if (this.email.hasError('required')) {
+    if (this.loginForm.controls.email.hasError('required')) {
       return 'You must enter a value';
     }
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+    return this.loginForm.controls.email.hasError('email')
+      ? 'Not a valid email'
+      : '';
   }
 
   getPasswordErrorMessage() {
-    if (this.password.hasError('required')) {
+    if (this.loginForm.controls.password.hasError('required')) {
       return 'You must enter a value';
     }
     return '';
+  }
+
+  onSub() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+      this.loginForm.markAllAsTouched();
+      console.log('invalid');
+    }
   }
 }
