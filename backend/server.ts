cc6c7c958ceb612e2051f139801811bdf9f86cbe
ilan Moroz <118884837+ilan-moroz@users.noreply.github.com>
2023-06-25 19:33:23 +0300

    change the connection to mongoDB

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,9 @@
 import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
-import config from "./Utils/Config";
 import userRouter from "./Routes/userRoutes";
-import connect from "./Utils/dalMongoDB";
 import helmet from "helmet";
+import mongoose from "mongoose";
 
 // Create Server
 const server = express();
@@ -26,7 +25,16 @@ server.use(helmet());
 server.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 
 // Start the server
-server.listen(config.webPort, () => {
-  console.log(`listinging on http://${config.myHost}:${config.webPort}`);
-  connect();
-});
+mongoose
+  .connect(process.env.MONGO_URL!)
+  .then(() => {
+    server.listen(process.env.PORT, () =>
+      console.log(
+        `Connected to MongoDB,Server running on port: ${process.env.PORT}`
+      )
+    );
+  })
+  .catch((error) => {
+    console.log(`Cannot connect to database: ${error.message}`);
+    process.exit(1);
+  });
